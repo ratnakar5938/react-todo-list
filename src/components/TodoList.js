a020afde9f1eb8ff3499e181ef05ed89882c6ba7
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-11 10:30:13 +0530

    add and remove todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -12,14 +13,36 @@ function TodoList() {
 
         const newTodos = [todo, ...todos]
 
-        setTodos(newTodos)
+        setTodos(newTodos);
+        console.log(...todos)
     };
 
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+
     return (
-        <div>
+        <>
             <h1>What's the plan for today</h1>
             <TodoForm onSubmit={addTodo} />
-        </div>
+            <Todo
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+            />
+        </>
     )
 }
 
